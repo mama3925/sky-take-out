fcbfae58996d5dd0e21bb2bf3fa96b9cd1a7abf4
mama3925 <xuwuyuan2000@foxmail.com>
2024-06-27 10:42:22 +0200

    fix: The error in the setmealDishMapper.xml. Because I forgot a function in this configuration file. So there is problem when I tried to use the  startOrStop function.

--- sky-server/src/main/java/com/sky/mapper/SetmealDishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealDishMapper.java
@@ -1,5 +1,6 @@
 package com.sky.mapper;
 
+import com.sky.entity.Dish;
 import com.sky.entity.SetmealDish;
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
@@ -38,4 +39,5 @@ public interface SetmealDishMapper {
      */
     @Select("select * from setmeal_dish where setmeal_id = #{setmealId}")
     List<SetmealDish> getBySetmealId(Long setmealId);
+
 }
