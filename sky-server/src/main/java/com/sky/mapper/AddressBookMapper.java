6c0091a9f5b4f16db421d8cc944f3ad211ff0291
mama3925 <xuwuyuan2000@foxmail.com>
2024-07-03 22:56:26 +0200

    add: the address function. But unfinished

---
+++ sky-server/src/main/java/com/sky/mapper/AddressBookMapper.java
@@ -0,0 +1,51 @@
+package com.sky.mapper;
+
+import com.sky.entity.AddressBook;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
+public interface AddressBookMapper {
+    /**
+     * 条件查询
+     * @param addressBook
+     * @return
+     */
+    List<AddressBook> list(AddressBook addressBook);
+
+    /**
+     * 新增
+     * @param addressBook
+     */
+    @Insert("insert into address_book" +
+            " (user_id, consignee, phone, sex, province_code, province_name, city_code, city_name, district_code," +
+            " district_name, detail, label, isDefault)" +
+            " values (#{userId}, #{consignee}, #{phone}, #{sex}, #{province_code}, #{province_name}, #{cityCode}, #{cityName}," +
+            " #{districtCode}, #{districtName}, #{detail}, #{label}, #{isDefault})")
+    void insert(AddressBook addressBook);
+
+    /**
+     * 根据id查询
+     * @param id
+     * @return
+     */
+    @Select("select * from address_book where id = #{id}")
+    AddressBook getById(Long id);
+
+    /**
+     * 根据id修改
+     * @param addressBook
+     */
+    void update(AddressBook addressBook);
+
+    /**
+     * 根据id删除地址
+     * @param id
+     */
+    @Delete("delete from address_book where id = #{id}")
+    void deleteById(Long id);
+}
---
+++ sky-server/src/main/java/com/sky/service/impl/AddressBookServiceImpl.java
@@ -0,0 +1,66 @@
+package com.sky.service.impl;
+
+import com.sky.context.BaseContext;
+import com.sky.entity.AddressBook;
+import com.sky.mapper.AddressBookMapper;
+import com.sky.service.AddressBookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AddressBookServiceImpl implements AddressBookService {
+    @Autowired
+    private AddressBookMapper addressBookMapper;
+    /**
+     * 条件查询
+     *
+     * @param addressBook
+     * @return
+     */
+    @Override
+    public List<AddressBook> list(AddressBook addressBook) {
+        return addressBookMapper.list(addressBook);
+    }
+
+    /**
+     * 新增地址
+     *
+     * @param addressBook
+     */
+    @Override
+    public void save(AddressBook addressBook) {
+        addressBook.setUserId(BaseContext.getCurrentId());
+        addressBook.setIsDefault(0);
+        addressBookMapper.insert(addressBook);
+    }
+
+    /**
+     * 根据id查询
+     *
+     * @param id
+     * @return
+     */
+    @Override
+    public AddressBook getById(Long id) {
+        AddressBook addressBook = addressBookMapper.getById(id);
+        return addressBook;
+    }
+
+    @Override
+    public void update(AddressBook addressBook) {
+        addressBookMapper.update(addressBook);
+    }
+
+
+    /**
+     * 根据id删除地址
+     *
+     * @param id
+     */
+    @Override
+    public void deleteById(Long id) {
+        addressBookMapper.deleteById(id);
+    }
+}
