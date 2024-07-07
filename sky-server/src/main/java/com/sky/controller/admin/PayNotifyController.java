6254c63463762196d079af65ec313ec7aecffeca
mama3925 <xuwuyuan2000@foxmail.com>
2024-07-07 22:58:52 +0200

    add: the order payment function.

---
+++ sky-server/src/main/java/com/sky/controller/admin/PayNotifyController.java
@@ -0,0 +1,54 @@
+package com.sky.controller.admin;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.sky.properties.WeChatProperties;
+import com.sky.service.OrderService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+/**
+ * 支付回调相关接口
+ */
+@RestController
+@RequestMapping("/notify")
+@Slf4j
+public class PayNotifyController {
+    @Autowired
+    private OrderService orderService;
+    @Autowired
+    private WeChatProperties weChatProperties;
+
+    /**
+     * 支付成功回调
+     *
+     * @param request
+     */
+    @RequestMapping("/paySuccess")
+    public void paySuccessNotify(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        //读取数据
+        String body = readData(request);
+        log.info("支付成功回调：{}", body);
+        //数据解密
+        String plainText = decryptData(body);
+        log.info("解密后的文本：{}", plainText);
+
+        JSONObject jsonObject = JSON.parseObject(plainText);
+        String outTradeNo = jsonObject.getString("out_trade_no");//商户平台订单号
+        String transactionId = jsonObject.getString("transaction_id");//微信支付交易号
+
+        log.info("商户平台订单号：{}", outTradeNo);
+        log.info("微信支付交易号：{}", transactionId);
+
+        //业务处理，修改订单状态、来单提醒
+        orderService.paySuccess(outTradeNo);
+
+    }
+}
